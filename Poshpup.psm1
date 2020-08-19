5f334d3add0f6ac39d98f265cb7bf05b12223055
Spencer <SpencerSchilsky@gmail.com>
2020-08-19 16:17:50 -0400

    It lives!

--- Poshpup.psm1
+++ Poshpup.psm1
@@ -1,5 +1,5 @@
 
-function Test-APIKey{
+function Test-DDAPIKeys{
     param(
     [Parameter(Mandatory)]   
     [string]$APIKey
@@ -10,43 +10,59 @@ function Test-APIKey{
         'DD-API-KEY' = $APIKey 
     }
     try{if((Invoke-RestMethod -Method Get -URI "https://api.datadoghq.com/api/v1/validate" -Headers $header).valid -eq "True") {Write-Output "The API Key is Valid"}}
-    catch{Write-Output "The API Key provided is invalid."}
+    catch{Write-Output "The API Key provided is invalid." -ErrorAction Stop}
     
 }
 
 function Set-DDAPIKeys{
     param(
-    [Parameter(Mandatory)]   
+    [Parameter(Mandatory,HelpMessage="Enter the API key")]   
     [string]$APIKey,
     
-    [Parameter(Mandatory)]   
+    [Parameter(Mandatory,HelpMessage="Enter the Application Key")]   
     [string]$ApplicationKey
     )
 
     #Tests the API Key before continuing
-    Test-APIKey $APIKey
+    Test-DDAPIKeys $APIKey
 
     #Sets up headers
-    $keys = @{
+    $global:keys = @{
         'DD-API-KEY' = $APIKey
         'DD-APPLICATION-KEY' = $ApplicationKey
         }    
 }
 
-function Set-DDAIJSON{
+function Get-DDAPIKeys{
+    if($global:keys){$global:keys}
+    else{"No Keys have been configured"}
+}
+
+function Send-DDAPIMetric{
     param(
-    [Parameter()]
+    [Parameter(Mandatory,HelpMessage="Enter the hostname for the Metric")]
     [string]$hostname,
 
+    [Parameter(Mandatory,HelpMessage="Enter the name of the Metric")]
+    [string]$metricName,
+
     [Parameter()]
+    [int]$time,
+
+    [Parameter(Mandatory,HelpMessage="Enter the value of Metric")]
     [string]$metric,
 
     [Parameter()]
-    [string]$host,
+    [string]$tags,
 
     [Parameter()]
-    [string]$host,
+    [string]$type
+
     )
+
+    if(-not($keys)){
+        Write-Output "Please use Set-DDAPIKeys to configure the API and APP Keys" -ErrorAction Stop}
+    
     $json = '{"series": [ {
         "host": "",
         "metric": "",
@@ -55,9 +71,34 @@ function Set-DDAIJSON{
                 "timestamp",
                 "metric"]],
         "tags": "",
-        "type": "gauge"
+        "type": ""
             }]}' | ConvertFrom-Json
+
+    $json.series[0].host = $hostname
+    $json.series[0].metric = $metricName
+    if($time){$json.series.points[0] = $time}
+    else{$json.series.points[0] = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))}
+    $json.series.points[1] = $metric
+    if($tags){$json.series[0].tags = $tags}
+    else{$tags = ""}
+    
+    #Splatts the Web Request
+    $params = @{
+		Uri = 'https://api.datadoghq.com/api/v1/series'
+		Method = 'Post'
+		Headers = $global:keys
+		Body = $json | ConvertTo-Json -Depth 4
+		ContentType = 'application/json'
+		}
+
+	try{Invoke-RestMethod @params}
+	catch{Write-Output -ErrorAction Stop "Failed to Invoke-Rest Method with $Params, investigate."}
+    Write-Output "The following metric was submitted to DD via API:"
+    Write-Output $json | ConvertTo-Json -Depth 4
+    
 }
-Export-ModuleMember -Function Test-APIKey
+
+Export-ModuleMember -Function Test-DDAPIKeys
 Export-ModuleMember -Function Set-DDAPIKeys
-Export-ModuleMember -Function Set-DDAIJSON
+Export-ModuleMember -Function Get-DDAPIKeys
+Export-ModuleMember -Function Send-DDAPIMetric
